fa152ee87971ff84a1cfc73a80666e3e8a14beb8
suresh <suresh@gmail.com>
2026-02-11 06:35:50 +0000

    testing githook/webhook

--- demo.py
+++ demo.py
@@ -4,3 +4,7 @@ This is for testing build remotely using token name as admin
 
 Testing below job
 3) New job created build after project build
+
+
+5) Added new job githook or webhook
+in github repo setting need to set payload as jenkin Baseurl "url":8080
