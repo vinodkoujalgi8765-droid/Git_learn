17193cb11adb61eb515e01b031c99b312977f4f6
suresh <suresh@gmail.com>
2026-02-11 06:42:35 +0000

    confirmung one build for multiple commits/push in build6 onjob2scm

--- job2_pollscm_code.py
+++ job2_pollscm_code.py
@@ -1,2 +1,8 @@
 This is tested to confirm poll_scm sheduler
 This should be auto generate job once commited code and push into GitHub
+
+
+
+As of now total 5 jobs are created #wed 11-02-2026 12PM
+After this demo script has added new code and push
+Now changing this script as second commit, hopes both commits are available in same build i.e6
